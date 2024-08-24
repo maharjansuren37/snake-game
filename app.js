33a24dbedcad5bf78fc86b2c3d505986d7e39fe8
maharjansuren37 <maharjansuren37@gmail.com>
2024-08-25 06:10:58 +0930

    add snake

--- app.js
+++ app.js
@@ -47,20 +47,51 @@ const UICtrl = (function () {
         ctx.strokeRect(10, 10, 10, 10);
     }
 
+    const drawSnake = () => {
+
+        for (let i = 0; i < 3; i++) {
+            drawSnakePart(10);
+        }
+    }
+
+    const drawSnakePart = (w) => {
+        ctx.fillStyle = gameContent.snakeColor;
+        ctx.strokeStyle = gameContent.snakeBorderColor;
+
+        ctx.fillRect(100, 100, 10, 10);
+        ctx.strokeRect(100, 100, 10, 10);
+    }
+
+    const drawSnakePart2 = () => {
+        ctx.fillStyle = gameContent.snakeColor;
+        ctx.strokeStyle = gameContent.snakeBorderColor;
+
+        ctx.fillRect(110, 100, 10, 10);
+        ctx.strokeRect(110, 100, 10, 10);
+    }
+
     return {
         getSelectors: function() {
             return UISelectors;
         },
         drawCanvas: drawCanvas,
+        drawFood: drawFood,
+        drawSnake: drawSnake
 
     }
 })();
 
 // App controller
 const app = (function(UserCtrl, StorageCtrl, UICtrl) {
+    const draw = function() {
+        UICtrl.drawCanvas();
+        UICtrl.drawFood();
+        UICtrl.drawSnake();
+    }
+
     // load event listeners
     const loadEventListeners = () => {
-        window.addEventListener("load", UICtrl.drawCanvas);
+        window.addEventListener("load", draw);
     }
 
     // UICtrl.draw();
